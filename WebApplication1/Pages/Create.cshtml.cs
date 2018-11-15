0c20e2be27dfa217787021ab976d91bb617ecc78
valentinakoleva <valentinakoleva98@gmail.com>
2018-11-15 13:14:01 +0200

    my homeworks

---
+++ WebApplication1/Pages/Create.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace WebApplication1.Pages
+{
+    public class CreateModel : PageModel
+    {
+        private readonly AppDbContext _db;
+
+            [BindProperty]
+        public Customer Customer { get; set; }
+        private ILogger<CreateModel> _log;
+        public CreateModel(AppDbContext db, ILogger<CreateModel> log)
+        {
+            _db = db;
+            _log = log;
+        }
+        [TempData]
+        public string Message { get; set; }
+
+        
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            _db.Customers.Add(Customer);
+
+            await _db.SaveChangesAsync();
+           var msg = $"Customer{Customer.Name} added.";
+            Message = msg;
+
+            _log.LogCritical(msg);
+
+
+            return RedirectToPage("/Index");
+
+        }
+    }
+}
